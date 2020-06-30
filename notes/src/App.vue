2a73ec11039c02a43c013146e5456afae89cc2ba
kirill.ilinsky <kirill.ilinsky@gmail.com>
2020-06-30 14:02:25 +0300

    add pror styles (scss)

--- notes/src/App.vue
+++ notes/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
   </div>
 </template>
 
@@ -10,9 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+   
 }
 </script>
 
